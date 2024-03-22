95621e6220a5bee64440e3c423b37ec8d2ef2c1a
Norpheus Studios <142515868+norpheusstudio@users.noreply.github.com>
2024-03-22 19:19:26 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+function confirmbtn() {
+  alert('By clicking the OK, you agreed with the terms and conditions.')
+}
+function inDev() {
+  alert('This feature is currently under development –Developer');
+}
+function menu() {
+  document.getElementById('index').innerHTML = 'This is Menu';
+}
+function about() {
+  document.getElementById('index').innerHTML = 'This is About'
+}
+function blog() {
+
+}
+function contact() {
+  
+}
